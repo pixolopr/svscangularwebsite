e4bc73c4651a78491ed47e5b01a3889472501a0a
Abhay Amin <pixoloproductions@gmail.com>
2014-12-23 18:58:14 +0530

    files

---
+++ js/controllers.js
@@ -0,0 +1,48 @@
+var phonecatControllers = angular.module('phonecatControllers', ['templateservicemod', 'navigationservice']);
+
+phonecatControllers.controller('home', ['$scope', 'TemplateService', 'NavigationService',
+  function ($scope, TemplateService, NavigationService) {
+        $scope.template = TemplateService;
+        $scope.menutitle = NavigationService.makeactive("Home");
+        TemplateService.title = "OMKAR";
+        $scope.navigation = NavigationService.getnav();
+      
+        TemplateService.content = "views/content.html";
+        console.log($scope.content);
+  }]);
+
+
+phonecatControllers.controller('about', ['$scope', 'TemplateService', 'NavigationService',
+  function ($scope, TemplateService, NavigationService) {
+        $scope.template = TemplateService;
+        $scope.menutitle = NavigationService.makeactive("About");
+        TemplateService.title = $scope.menutitle;
+        $scope.navigation = NavigationService.getnav();
+  }]);
+phonecatControllers.controller('services', ['$scope', 'TemplateService', 'NavigationService',
+  function ($scope, TemplateService, NavigationService) {
+        $scope.template = TemplateService;
+        $scope.menutitle = NavigationService.makeactive("Services");
+        TemplateService.title = $scope.menutitle;
+        $scope.navigation = NavigationService.getnav();
+  }]);
+phonecatControllers.controller('portfolio', ['$scope', 'TemplateService', 'NavigationService',
+  function ($scope, TemplateService, NavigationService) {
+        $scope.template = TemplateService;
+        $scope.menutitle = NavigationService.makeactive("Portfolio");
+        TemplateService.title = $scope.menutitle;
+        $scope.navigation = NavigationService.getnav();
+  }]);
+phonecatControllers.controller('contact', ['$scope', 'TemplateService', 'NavigationService',
+  function ($scope, TemplateService, NavigationService) {
+        $scope.template = TemplateService;
+        $scope.menutitle = NavigationService.makeactive("Contact");
+        TemplateService.title = $scope.menutitle;
+        $scope.navigation = NavigationService.getnav();
+  }]);
+
+
+phonecatControllers.controller('headerctrl', ['$scope', 'TemplateService',
+ function ($scope, TemplateService) {
+        $scope.template = TemplateService;
+  }]);
